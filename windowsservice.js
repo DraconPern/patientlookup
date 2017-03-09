97ddb2ca8cf1a0ecd1ce61b29009420b651f9a0b
DraconPern <draconpern@hotmail.com>
2017-03-09 15:33:15 -0600

    added windows service

---
+++ windowsservice.js
@@ -0,0 +1,17 @@
+var Service = require('node-windows').Service;
+
+// Create a new service object
+var svc = new Service({
+  name:'patientlookup',
+  description: 'patientlookup',
+  script: require('path').join(__dirname,'bin/www.js')
+
+});
+
+// Listen for the "install" event, which indicates the
+// process is available as a service.
+svc.on('install',function(){
+  svc.start();
+});
+
+svc.install();
